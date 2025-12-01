e2e9c23e37afffa13115ddefc824ffa64f63b884
Zinah <zinah.poulus@gmail.com>
2025-12-01 13:17:32 +0100

    Flask init

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine, URL, text
+from sqlalchemy.orm import sessionmaker
+
+url = URL.create(
+    drivername="postgresql+psycopg2",
+    host="localhost",
+    port=5432,
+    username="postgres",
+    password="2008",
+    database="auction_furniture"
+)
+
+engine = create_engine(url)
+
+app = Flask(__name__)
+Session = sessionmaker(bind=engine)
+
+@app.get('/users')
+def get_users():
+    with Session() as session:
+        result = session.execute(text("SELECT * FROM users")).fetchall()
+        users_list = [
+            {
+                "id": row.id,
+                "f_name": row.f_name,
+                "l_name": row.l_name,
+                "phone": row.phone,
+                "email": row.email
+            } for row in result
+        ]
+
+    return jsonify(users_list), 200
+
